7e9085f22baed6dd4f6ec8ca01b0b899ed4b1b86
Milaimac <1099843695@qq.com>
2021-09-03 14:51:40 +0800

    要实现二叉树的镜像，就是要递归地把左右子树交换

---
+++ 剑指offer/MirroTree.cpp
@@ -0,0 +1,25 @@
+// 剑指 Offer 27. 二叉树的镜像
+// 递归
+#include<iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(root==nullptr){
+            return nullptr;
+        }
+
+        TreeNode* left = mirrorTree(root->left);
+        TreeNode* right = mirrorTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
