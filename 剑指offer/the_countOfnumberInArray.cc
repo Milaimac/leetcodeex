5b3c1cee039034c31078401dce4c181a9bc1d934
Milaimac <1099843695@qq.com>
2021-09-16 23:25:02 +0800

    考察位运算

---
+++ 剑指offer/the_countOfnumberInArray.cc
@@ -0,0 +1,50 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> singleNumbers(vector<int>& nums) {
+        vector<int> res;
+        sort(nums.begin(),  nums.end());
+        for(int i = 0;i<nums.size()-1;++i){
+            if(nums[i] == nums[i+1]){
+                continue;
+            }
+            else if(i > 0 || nums[i] == nums[i-1]){
+                continue;
+            }
+            else{
+                res.push_back(nums[i]);
+            }
+        }
+        return res;
+    }
+};
+// 查找出只出现一次的数字
+// 排序，然后判断？
+// 但是要求时间复杂度为O(n),空间复杂度为O(1)
+// 只不过没有想到，官方题解是使用异或来达成的
+
+class Solutionv1 {
+public:
+    vector<int> singleNumbers(vector<int>& nums) {
+        int n = 0;
+        for(auto& num:nums){
+            n ^= num;
+        }
+        int m = 1;
+        while((n & m) == 0){
+            m <<= 1;
+        }
+        int x = 0, y = 0;
+        for(auto& num:nums){
+            if(num & m){
+                x ^= num;
+            }else{
+                y ^= num;
+            }
+        }
+        return vector<int> {x, y};
+    }
+};
