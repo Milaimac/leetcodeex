bc167e08815ccf5c976fff0e1a714090160a5361
Milaimac <1099843695@qq.com>
2021-11-01 10:43:41 +0800

    添加了题目

--- 前缀和/Continuous_Subarray_Sum.cpp
+++ 前缀和/Continuous_Subarray_Sum.cpp
@@ -1,7 +1,17 @@
 // 523. 连续的子数组和
+// 给你一个整数数组 nums 和一个整数 k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+
+//     子数组大小 至少为 2 ，且
+//     子数组元素总和为 k 的倍数。
+
+// 如果存在，返回 true ；否则，返回 false 。
+
+// 如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。0 始终视为 k 的一个倍数。
 
 // 要求的是子数组总和为k的倍数
-#include"head.h"
+#include<vector>
+#include<unordered_map>
+using namespace std;
 class Solution{
 public: 
     bool checkSubarraySum(vector<int>& nums, int k){
